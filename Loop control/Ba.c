ed4fcc74065d07887381cb47bfc7c95369710f58
Siddhi-Bodake <117656009+Siddhi-Bodake@users.noreply.github.com>
2022-11-07 19:55:48 +0530

    Create Ba.c

---
+++ Loop control/Ba.c
@@ -0,0 +1,14 @@
+//all ascii value and equivalent character by while loop//
+
+#include<stdio.h>
+
+int main()
+{
+    char ascii;
+    int i;
+
+    for(i=0;i<=255;i++)
+    {
+        printf("%c = %d\n", i, i);
+    }
+}
